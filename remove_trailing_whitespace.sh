675bd054ce572c67cb916e31af5cf77c534202c9
Keshav Amburay <the.ridikulus.rat@gmail.com>
2010-09-21 15:08:31 +0530

    Major changes

--- remove_trailing_whitespace.sh
+++ remove_trailing_whitespace.sh
@@ -1,13 +1,42 @@
 #!/bin/bash
+	
+strip_whitespace() {
+	
+echo
+echo "Strip trailing white spaces from ${PWD}/${fil}"
+echo
 
-# strip trailing white spaces.
-for ext in "sh"
-do
-  find . -type f -name "*.${ext}" -exec sed -i "s/[[:space:]]*$//" {} \;
-done
+## strip trailing white spaces.
+sed -i "s/[[:space:]]*$//" ${PWD}/${fil}
 
-# ensures all files end with a newline.
-for ext in "sh"
+## ensures all files end with a newline.
+find . -type f -name "${fil}" -print | xargs printf "e %s\nw\n" | ed -s;
+
+echo
+
+}
+
+
+run() {
+
+fil=""
+ls -1 | while read -r fil
 do
-  find . -type f -name "*.${ext}" -print | xargs printf "e %s\nw\n" | ed -s;
+  if [ `file -b --mime-type ${PWD}/${fil}` == "text/*" ]
+  then
+      strip_whitespace
+  elif [ -d "${PWD}/${fil}" ]
+  then
+      pushd "${fil}" > /dev/null
+	  run			
+	  popd > /dev/null
+  fi       
 done
+	
+}
+
+set -x -e
+
+run
+
+set +x +e
