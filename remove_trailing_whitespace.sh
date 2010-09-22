f37158c3c7fef33beca41ad3f505da910ee5ff33
Keshav Amburay <the.ridikulus.rat@gmail.com>
2010-09-22 18:51:52 +0530

    Reset script

--- remove_trailing_whitespace.sh
+++ remove_trailing_whitespace.sh
@@ -1,44 +1,13 @@
 #!/bin/bash
 
-strip_whitespace() {
-
-echo
-echo "Strip trailing white spaces from ${PWD}/${fil}"
-echo
-
-## strip trailing white spaces.
-sed -i "s/[[:space:]]*$//" ${PWD}/${fil}
-
-## ensures all files end with a newline.
-find . -type f -name "${fil}" -print | xargs printf "e %s\nw\n" | ed -s;
-
-echo
-
-}
-
-
-run() {
-
-fil=""
-ls -1 | while read -r fil
+# strip trailing white spaces.
+for ext in "sh"
 do
-  file_mime=`file -b --mime-type ${PWD}/${fil}`
-
-  if [ "${file_mime}" == "text/plain" ] || [ "${file_mime}" == "text/x-shellscript" ]
-  then
-      strip_whitespace
-  elif [ -d "${PWD}/${fil}" ]
-  then
-      pushd "${fil}" > /dev/null
-	  run
-	  popd > /dev/null
-  fi
+  find . -type f -name "*.${ext}" -exec sed -i "s/[[:space:]]*$//" {} \;
 done
 
-}
-
-set -e
-
-run
-
-set +e
+# ensures all files end with a newline.
+for ext in "sh"
+do
+  find . -type f -name "*.${ext}" -print | xargs printf "e %s\nw\n" | ed -s;
+done
