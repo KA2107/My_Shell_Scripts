e0f3ab87b94c51d44fcf88d2b75e7d16d0b98516
Keshav Amburay <the.ridikulus.rat@gmail.com>
2011-01-16 13:44:51 +0530

    Update archlinux scripts

--- arch/unofficial_repos_pacman_git.sh
+++ arch/unofficial_repos_pacman_git.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 
+set -x
+
+sudo rm /var/lib/pacman/sync/*.part
+echo
+
 sudo rm /var/lib/pacman/sync/{radeon,nightly}.db
+echo
 
 sudo aria2c --dir=/var/lib/pacman/sync/ --out=radeon.db http://spiralinear.org/perry3d/x86_64/radeon.db.tar.gz
+echo
 
 sudo aria2c --dir=/var/lib/pacman/sync/ --out=nightly.db http://nightly.uhuc.de/x86_64/nightly.db.tar.gz
+echo
+
+set +x
---
+++ arch/update_pkgs.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -x
+
+sudo pacman -Sy
+echo
+
+${PWD}/unofficial_repos_pacman_git.sh
+echo
+
+sudo pacman -Su
+echo
+
+yaourt -Su
+echo
+
+sudo pacman -Sc
+echo
+
+set +x
