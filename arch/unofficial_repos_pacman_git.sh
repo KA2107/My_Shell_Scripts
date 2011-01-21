2574192c36a20131c8c4103c13a601d56139586c
Keshav Amburay <the.ridikulus.rat@gmail.com>
2011-01-21 19:44:26 +0530

    Update nightly repo url and remove radeon repo

--- arch/unofficial_repos_pacman_git.sh
+++ arch/unofficial_repos_pacman_git.sh
@@ -5,13 +5,13 @@ set -x
 sudo rm /var/lib/pacman/sync/*.part
 echo
 
-sudo rm /var/lib/pacman/sync/{radeon,nightly}.db
+sudo rm /var/lib/pacman/sync/nightly.db
 echo
 
-sudo aria2c --dir=/var/lib/pacman/sync/ --out=radeon.db http://spiralinear.org/perry3d/x86_64/radeon.db.tar.gz
-echo
+# sudo aria2c --dir=/var/lib/pacman/sync/ --out=radeon.db http://spiralinear.org/perry3d/x86_64/radeon.db.tar.gz
+# echo
 
-sudo aria2c --dir=/var/lib/pacman/sync/ --out=nightly.db http://nightly.uhuc.de/x86_64/nightly.db.tar.gz
+sudo aria2c --dir=/var/lib/pacman/sync/ --out=nightly.db http://files.shadowice.org/nightly/x86_64/nightly.db.tar.gz
 echo
 
 set +x
