d8a1189df6e3062349634184e0fa349b0a454b71
Keshav Amburay <the.ridikulus.rat@gmail.com>
2011-01-27 19:52:23 +0530

    git: Replace spaces with tabs

--- git/git_gc_aggressive.sh
+++ git/git_gc_aggressive.sh
@@ -4,24 +4,24 @@ set -e
 
 run()
 {
-  repo=""
-  ls --all -1 | while read -r repo
-  do
-    if [ -d "${PWD}/${repo}" ] && [ "${repo}" == ".git" ]
-    then
-        echo
-        echo "${PWD}"
-        echo
-        
-        git gc --aggressive
-        echo
-    elif [ -d "${PWD}/${repo}" ] && [ "${repo}" != "." ] && [ "${repo}" != ".." ]
-    then
-        pushd "${repo}" > /dev/null
-        run
-        popd > /dev/null
-    fi
-  done
+	repo=""
+	ls --all -1 | while read -r repo
+	do
+		if [ -d "${PWD}/${repo}" ] && [ "${repo}" == ".git" ]
+		then
+			echo
+			echo "${PWD}"
+			echo
+			
+			git gc --aggressive
+			echo
+		elif [ -d "${PWD}/${repo}" ] && [ "${repo}" != "." ] && [ "${repo}" != ".." ]
+		then
+			pushd "${repo}" > /dev/null
+			run
+			popd > /dev/null
+		fi
+	done
 }
 
 run
--- git/git_update.sh
+++ git/git_update.sh
@@ -2,85 +2,85 @@
 
 run()
 {
-  repo=""
-  ls --all -1 | while read -r repo
-  do
-    if [ -d "${PWD}/${repo}" ] && [ "${repo}" == ".git" ]
-    then
-        if [ -d "${PWD}/.git/svn" ]
-        then
-            echo
-            echo "GIT SVN - ${PWD}"
-            echo
-            
-            git reset --hard
-            echo
-            
-            git svn rebase --verbose
-            echo
-            
-            git checkout master
-            echo
-            
-            git merge remotes/git-svn
-            echo
-            
-            git reset --hard
-            echo
-            
-        elif [ -d "${PWD}/.git/bzr" ]
-        then
-            echo
-            echo "GIT BZR- ${PWD}"
-            echo
-            
-            git reset --hard
-            echo
-            
-            git bzr sync bzr/master
-            echo
-            
-            git checkout master
-            echo
-            
-            git merge bzr/master
-            echo
-            
-            git reset --hard
-            echo
-            
-        else
-            if [ -d "${PWD}/.git/refs/remotes" ]
-            then
-                echo
-                echo "GIT - ${PWD}"
-                echo
-                
-                git reset --hard
-                echo
-                
-                git fetch
-                echo
-            
-                git checkout master
-                echo
-                
-                git merge remotes/origin/master
-                echo
-                
-                git reset --hard
-                echo
-            fi
-            echo
-        fi
-            
-    elif [ -d "${PWD}/${repo}" ] && [ "${repo}" != "." ] && [ "${repo}" != ".." ]
-    then
-        pushd "${repo}" > /dev/null
-        run
-        popd > /dev/null
-    fi
-  done
+	repo=""
+	ls --all -1 | while read -r repo
+	do
+		if [ -d "${PWD}/${repo}" ] && [ "${repo}" == ".git" ]
+		then
+			if [ -d "${PWD}/.git/svn" ]
+			then
+				echo
+				echo "GIT SVN - ${PWD}"
+				echo
+				
+				git reset --hard
+				echo
+				
+				git svn rebase --verbose
+				echo
+				
+				git checkout master
+				echo
+				
+				git merge remotes/git-svn
+				echo
+				
+				git reset --hard
+				echo
+				
+			elif [ -d "${PWD}/.git/bzr" ]
+			then
+				echo
+				echo "GIT BZR- ${PWD}"
+				echo
+				
+				git reset --hard
+				echo
+				
+				git bzr sync bzr/master
+				echo
+				
+				git checkout master
+				echo
+				
+				git merge bzr/master
+				echo
+				
+				git reset --hard
+				echo
+				
+			else
+				if [ -d "${PWD}/.git/refs/remotes" ]
+				then
+					echo
+					echo "GIT - ${PWD}"
+					echo
+					
+					git reset --hard
+					echo
+					
+					git fetch
+					echo
+					
+					git checkout master
+					echo
+					
+					git merge remotes/origin/master
+					echo
+					
+					git reset --hard
+					echo
+				fi
+			fi
+			echo
+			
+		elif [ -d "${PWD}/${repo}" ] && [ "${repo}" != "." ] && [ "${repo}" != ".." ]
+		then
+			pushd "${repo}" > /dev/null
+			run
+			popd > /dev/null
+		fi
+	done
 }
 
 run
