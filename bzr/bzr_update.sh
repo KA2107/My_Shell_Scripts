d6f5f75a927b75b2b398b8f60706be60c85f5705
Keshav Amburay <the.ridikulus.rat@gmail.com>
2011-01-27 19:45:35 +0530

    bzr: Replace spaces with tabs

--- bzr/bzr_update.sh
+++ bzr/bzr_update.sh
@@ -2,29 +2,29 @@
 
 run()
 {
-  repo=""
-  ls --all -1 | while read -r repo
-  do
-    if [ -d "${PWD}/${repo}" ] && [ "${repo}" == ".bzr" ]
-    then
-        if [ $(echo "${PWD}" | grep '.git/bzr/repo') ] || [ $(echo "${PWD}" | grep '.git/bzr/repo/master') ]
-        then
-            true
-        else
-            echo
-            echo "BZR - ${PWD}"
-            echo
-            
-            bzr pull
-            echo
-        fi
-    elif [ -d "${PWD}/${repo}" ] && [ "${repo}" != "." ] && [ "${repo}" != ".." ]
-    then
-        pushd "${repo}" > /dev/null
-        run
-        popd > /dev/null
-    fi
-  done
+	repo=""
+	ls --all -1 | while read -r repo
+	do
+		if [ -d "${PWD}/${repo}" ] && [ "${repo}" == ".bzr" ]
+		then
+			if [ $(echo "${PWD}" | grep '.git/bzr/repo') ] || [ $(echo "${PWD}" | grep '.git/bzr/repo/master') ]
+			then
+				true
+			else
+				echo
+				echo "BZR - ${PWD}"
+				echo
+				
+				bzr pull
+				echo
+			fi
+		elif [ -d "${PWD}/${repo}" ] && [ "${repo}" != "." ] && [ "${repo}" != ".." ]
+		then
+			pushd "${repo}" > /dev/null
+			run
+			popd > /dev/null
+		fi
+	done
 }
 
 bzr lp-login skodabenz
