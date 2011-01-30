5481fb794f760ac0a372d2cc6cfaee32e3b48281
Keshav Amburay <the.ridikulus.rat@gmail.com>
2011-01-30 11:06:49 +0530

    Added more grub2 specific files

--- grub2/compile_grub2.sh
+++ grub2/compile_grub2.sh
@@ -109,95 +109,95 @@ fi
 
 if [ "${PROCESS_CONTINUE_UEFI}" == "TRUE" ]
 then
-
+	
 	set -x -e
-
+	
 	# ${WD_Outer}/xman_dos2unix.sh * || true
-
+	
 	## First compile GRUB2 for UEFI x86_64
-
+	
 	rm -rf ${WD_Outer}/${GRUB2_EFI_Source_DIR_Name}/grub2_extras__GIT_BZR || true
-
+	
 	cp -r ${WD_Outer}/grub2_extras__GIT_BZR ${WD_Outer}/${GRUB2_EFI_Source_DIR_Name}/grub2_extras__GIT_BZR || true
 	rm -rf ${WD_Outer}/${GRUB2_EFI_Source_DIR_Name}/grub2_extras__GIT_BZR/zfs || true
 	rm -rf ${WD_Outer}/${GRUB2_EFI_Source_DIR_Name}/grub2_extras__GIT_BZR/915resolution || true
 	rm -rf ${WD_Outer}/${GRUB2_EFI_Source_DIR_Name}/grub2_extras__GIT_BZR/ntldr-img || true
-
+	
 	if [ "${GRUB2_EFI_Source_DIR_Name}" != "${GRUB2_BIOS_Source_DIR_Name}" ]
 	then
 		cd ${WD_Outer}/${GRUB2_EFI_Source_DIR_Name}/
 		echo
 	fi
-
+	
 	cp --verbose ${WD_Outer}/xman_dos2unix.sh ${WD_Outer}/grub.default ${WD_Outer}/${GRUB2_EFI_Source_DIR_Name}/ || true
 	cp --verbose ${WD_Outer}/grub2_efi_linux_scripts/grub2_efi.sh ${WD_Outer}/grub2_efi_linux_scripts/grub2_efi_linux_my.sh ${WD_Outer}/${GRUB2_EFI_Source_DIR_Name}/
 	echo
-
+	
 	rm ${WD_Outer}/${GRUB2_EFI_Source_DIR_Name}/grub.cfg || true
 	cp --verbose ${WD_Outer}/grub2_efi_linux_scripts/grub2_efi.cfg ${WD_Outer}/${GRUB2_EFI_Source_DIR_Name}/grub.cfg || true
 	echo
-
+	
 	cd ${WD_Outer}/${GRUB2_EFI_Source_DIR_Name}/
 	${PWD}/grub2_efi_linux_my.sh
 	echo
 	cd ..
-
+	
 	cp -r ${WD_Outer}/${GRUB2_EFI_Source_DIR_Name}/GRUB2_EFI_BUILD_DIR_x86_64 ${WD_Outer}/GRUB2_EFI_BUILD_DIR_x86_64 || true
 	sudo rm -rf ${WD_Outer}/${GRUB2_EFI_Source_DIR_Name}/GRUB2_EFI_BUILD_DIR_x86_64 || true
 	echo
-
+	
 	set +x +e
-
+	
 fi
 
 if [ "${PROCESS_CONTINUE_BIOS}" = "TRUE" ]
 then
-
+	
 	set -x -e
-
+	
 	# ${WD_Outer}/xman_dos2unix.sh * || true
-
+	
 	## Second compile GRUB2 for BIOS
-
+	
 	rm -rf ${WD_Outer}/${GRUB2_BIOS_Source_DIR_Name}/grub2_extras__GIT_BZR || true
-
+	
 	cp -r ${WD_Outer}/grub2_extras__GIT_BZR ${WD_Outer}/${GRUB2_BIOS_Source_DIR_Name}/grub2_extras__GIT_BZR || true
 	rm -rf ${WD_Outer}/${GRUB2_BIOS_Source_DIR_Name}/grub2_extras__GIT_BZR/zfs || true
 	rm -rf ${WD_Outer}/${GRUB2_BIOS_Source_DIR_Name}/grub2_extras__GIT_BZR/915resolution || true
-
+	
 	if [ "${GRUB2_BIOS_Source_DIR_Name}" != "${GRUB2_EFI_Source_DIR_Name}" ]
 	then
 		cd ${WD_Outer}/${GRUB2_BIOS_Source_DIR_Name}/
 		echo
 	fi
-
+	
 	cp --verbose ${WD_Outer}/xman_dos2unix.sh ${WD_Outer}/grub.default ${WD_Outer}/${GRUB2_BIOS_Source_DIR_Name}/ || true
 	cp --verbose ${WD_Outer}/grub2_bios_linux_scripts/grub2_bios.sh ${WD_Outer}/grub2_bios_linux_scripts/grub2_bios_linux_my.sh ${WD_Outer}/${GRUB2_BIOS_Source_DIR_Name}/
 	echo
-
+	
 	rm ${WD_Outer}/${GRUB2_BIOS_Source_DIR_Name}/grub.cfg || true
 	cp --verbose ${WD_Outer}/grub2_bios_linux_scripts/grub2_bios.cfg ${WD_Outer}/${GRUB2_BIOS_Source_DIR_Name}/grub.cfg || true
 	echo
-
+	
 	cd ${WD_Outer}/${GRUB2_BIOS_Source_DIR_Name}/
-
+	
 	## CHROOT into the arch32 system for compiling GRUB2 BIOS i386
 	# export BCHROOT_DIR=${WD_Outer}
 	# sudo chroot ${x32_chroot} || return 1
 	# cd ${BCHROOT_DIR}
 	# unset BCHROOT_DIR
-
+	
 	# schroot --automatic-session --preserve-environment --directory ${WD_Outer}
 	${PWD}/grub2_bios_linux_my.sh
 	echo
 	cd ..
-
+	
 	cp -r ${WD_Outer}/${GRUB2_BIOS_Source_DIR_Name}/GRUB2_BIOS_BUILD_DIR ${WD_Outer}/GRUB2_BIOS_BUILD_DIR || true
 	sudo rm -rf ${WD_Outer}/${GRUB2_BIOS_Source_DIR_Name}/GRUB2_BIOS_BUILD_DIR || true
 	echo
-
+	
 	set +x +e
-
+	
 fi
 
 unset PROCESS_CONTINUE_UEFI
