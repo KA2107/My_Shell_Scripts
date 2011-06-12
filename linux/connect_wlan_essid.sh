1bb4837f10d49f25422f1d90307e3cdf0e6e465b
Keshav Amburay <the.ridikulus.rat@gmail.com>
2011-06-12 22:27:49 +0530

    Add iproute2 support to connect_wlan script

--- linux/connect_wlan_essid.sh
+++ linux/connect_wlan_essid.sh
@@ -2,34 +2,48 @@
 
 echo
 
+set -x -e
+
 ESSID=${1}
+ENCRYPTION_TYPE=${2}
+PASSPHRASE=${3}
 
-set +x +e
-
-sudo ip link set wlan0 down || true
-# sudo ifconfig wlan0 down || true
 echo
 
 sudo ip link set wlan0 down || true
 # sudo ifconfig wlan0 down || true
+
+echo
+
+sudo ip link set wlan0 down || true
+# sudo ifconfig wlan0 down || true
+
 echo
 
 sudo ip link set wlan0 up
 # sudo ifconfig wlan0 up
+
+echo
+
+sudo iwconfig
+
+echo
+
+sudo iwlist scan
+
 echo
 
 sudo iwconfig wlan0 essid "${ESSID}"
+
 echo
 
 sudo iwconfig | grep -i ESSID
+
 echo
 
 sudo dhclient -4 wlan0
 # sudo dhcpcd wlan0
+
 echo
 
-echo $?
-
 set +x +e
-
-exit 0
