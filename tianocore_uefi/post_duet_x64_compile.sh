eabcbe916079c4442cfd318716a35ab90f9fd66c
Keshav Amburay <the.ridikulus.rat@gmail.com>
2011-06-19 01:33:58 +0530

    Rename some of the tianocore_uefi bash scripts env vars

--- tianocore_uefi/post_duet_x64_compile.sh
+++ tianocore_uefi/post_duet_x64_compile.sh
@@ -15,8 +15,8 @@ MIGLE_BOOTDUET_COPY() {
 	
 	echo
 	
-	rm -rf "${EFI_DUET_GIT_DIR}/BootSector"/bd*.bin || true
-	cp "${MIGLE_BOOTDUET_COMPILE_DIR}"/bd*.bin "${EFI_DUET_GIT_DIR}/BootSector/"
+	rm -rf "${UEFI_DUET_INSTALLER_DIR}/BootSector"/bd*.bin || true
+	cp "${MIGLE_BOOTDUET_COMPILE_DIR}"/bd*.bin "${UEFI_DUET_INSTALLER_DIR}/BootSector/"
 	
 	echo
 	
@@ -24,13 +24,13 @@ MIGLE_BOOTDUET_COPY() {
 	
 	echo
 	
-	rm -rf "${EFI_DUET_GIT_DIR}/Migle_BootDuet_INSTALL.txt" || true
-	cp "${MIGLE_BOOTDUET_COMPILE_DIR}/INSTALL" "${EFI_DUET_GIT_DIR}/Migle_BootDuet_INSTALL.txt"
+	rm -rf "${UEFI_DUET_INSTALLER_DIR}/Migle_BootDuet_INSTALL.txt" || true
+	cp "${MIGLE_BOOTDUET_COMPILE_DIR}/INSTALL" "${UEFI_DUET_INSTALLER_DIR}/Migle_BootDuet_INSTALL.txt"
 	
 	echo
 	
-	rm -rf "${EFI_DUET_GIT_DIR}/Licenses/Migle_BootDuet_LICENSE.txt" || true
-	cp "${MIGLE_BOOTDUET_COMPILE_DIR}/COPYING" "${EFI_DUET_GIT_DIR}/Licenses/Migle_BootDuet_LICENSE.txt"
+	rm -rf "${UEFI_DUET_INSTALLER_DIR}/Licenses/Migle_BootDuet_LICENSE.txt" || true
+	cp "${MIGLE_BOOTDUET_COMPILE_DIR}/COPYING" "${UEFI_DUET_INSTALLER_DIR}/Licenses/Migle_BootDuet_LICENSE.txt"
 	
 	echo
 	
@@ -40,18 +40,18 @@ SRS5694_DUET_INSTALL() {
 	
 	echo
 	
-	rm -rf "${EFI_DUET_GIT_DIR}/duet-install" || true
-	cp "${SRS5694_DUET_INSTALL_DIR}/duet-install" "${EFI_DUET_GIT_DIR}/duet-install"
+	rm -rf "${UEFI_DUET_INSTALLER_DIR}/duet-install" || true
+	cp "${SRS5694_DUET_INSTALL_DIR}/duet-install" "${UEFI_DUET_INSTALLER_DIR}/duet-install"
 	
 	echo
 	
-	rm -rf "${EFI_DUET_GIT_DIR}/duet-install.8" || true
-	cp "${SRS5694_DUET_INSTALL_DIR}/duet-install.8" "${EFI_DUET_GIT_DIR}/duet-install.8"
+	rm -rf "${UEFI_DUET_INSTALLER_DIR}/duet-install.8" || true
+	cp "${SRS5694_DUET_INSTALL_DIR}/duet-install.8" "${UEFI_DUET_INSTALLER_DIR}/duet-install.8"
 	
 	echo
 	
-	rm -rf "${EFI_DUET_GIT_DIR}/Licenses/srs5694_duet-install_LICENSE.txt" || true
-	cp "${SRS5694_DUET_INSTALL_DIR}/COPYING" "${EFI_DUET_GIT_DIR}/Licenses/srs5694_duet-install_LICENSE.txt"
+	rm -rf "${UEFI_DUET_INSTALLER_DIR}/Licenses/srs5694_duet-install_LICENSE.txt" || true
+	cp "${SRS5694_DUET_INSTALL_DIR}/COPYING" "${UEFI_DUET_INSTALLER_DIR}/Licenses/srs5694_duet-install_LICENSE.txt"
 	
 	echo
 	
@@ -63,19 +63,19 @@ EFI_DUET_GIT() {
 	echo "EFI_DUET_GIT"
 	echo
 	
-	rm -rf "${EFI_DUET_GIT_DIR}/Efildr/UDK_X64/Efildr20" || true
-	cp "${DUET_EMUVARIABLE_BUILD_DIR}/FV/Efildr20" "${EFI_DUET_GIT_DIR}/Efildr/UDK_X64/Efildr20"
+	rm -rf "${UEFI_DUET_INSTALLER_DIR}/Efildr/UDK_X64/Efildr20" || true
+	cp "${DUET_EMUVARIABLE_BUILD_DIR}/FV/Efildr20" "${UEFI_DUET_INSTALLER_DIR}/Efildr/UDK_X64/Efildr20"
 	
 	echo
 	
-	rm -rf "${EFI_DUET_GIT_DIR}/Efildr/UDK_X64/Efildr20_FSVariable" || true
-	cp "${DUET_FSVARIABLE_BUILD_DIR}/FV/Efildr20" "${EFI_DUET_GIT_DIR}/Efildr/UDK_X64/Efildr20_FSVariable"
+	rm -rf "${UEFI_DUET_INSTALLER_DIR}/Efildr/UDK_X64/Efildr20_FSVariable" || true
+	cp "${DUET_FSVARIABLE_BUILD_DIR}/FV/Efildr20" "${UEFI_DUET_INSTALLER_DIR}/Efildr/UDK_X64/Efildr20_FSVariable"
 	
 	echo
 	
-	rm -rf "${EFI_DUET_GIT_DIR}/Shell/UDK_X64/Shell_Full.efi" || true
-	cp "${EDK2_DIR}/ShellBinPkg/UefiShell/X64/Shell.efi" "${EFI_DUET_GIT_DIR}/Shell/UDK_X64/Shell_Full.efi"
-	cp "${EDK2_DIR}/EdkShellBinPkg/FullShell/X64/Shell_Full.efi" "${EFI_DUET_GIT_DIR}/Shell/UDK_X64/Shell_Full_old.efi"
+	rm -rf "${UEFI_DUET_INSTALLER_DIR}/Shell/UDK_X64/Shell_Full.efi" || true
+	cp "${EDK2_DIR}/ShellBinPkg/UefiShell/X64/Shell.efi" "${UEFI_DUET_INSTALLER_DIR}/Shell/UDK_X64/Shell_Full.efi"
+	cp "${EDK2_DIR}/EdkShellBinPkg/FullShell/X64/Shell_Full.efi" "${UEFI_DUET_INSTALLER_DIR}/Shell/UDK_X64/Shell_Full_old.efi"
 	
 	echo
 	
@@ -87,28 +87,28 @@ EFI_DUET_GIT() {
 	
 	echo
 	
-	rm -rf "${EFI_DUET_GIT_DIR}/BootSector"/{bs32,Gpt,Mbr}.com || true
-	cp "${EDK2_DUET_BOOTSECT_BIN_DIR}"/{bs32,Gpt,Mbr}.com "${EFI_DUET_GIT_DIR}/BootSector/"
+	rm -rf "${UEFI_DUET_INSTALLER_DIR}/BootSector"/{bs32,Gpt,Mbr}.com || true
+	cp "${EDK2_DUET_BOOTSECT_BIN_DIR}"/{bs32,Gpt,Mbr}.com "${UEFI_DUET_INSTALLER_DIR}/BootSector/"
 	
 	echo
 	
-	rm -rf "${EFI_DUET_GIT_DIR}/Linux_Source/C" || true
-	cp -r "${EDK2_C_SOURCE_DIR}" "${EFI_DUET_GIT_DIR}/Linux_Source/C"
+	rm -rf "${UEFI_DUET_INSTALLER_DIR}/Linux_Source/C" || true
+	cp -r "${EDK2_C_SOURCE_DIR}" "${UEFI_DUET_INSTALLER_DIR}/Linux_Source/C"
 	
 	echo
 	
-	rm -rf "${EFI_DUET_GIT_DIR}/Windows_Binaries"/{BootSectImage,GenBootSector}.exe || true
-	cp "${EDK2_DIR}/BaseTools/Bin/Win32"/{BootSectImage,GenBootSector}.exe "${EFI_DUET_GIT_DIR}/Windows_Binaries/"
+	rm -rf "${UEFI_DUET_INSTALLER_DIR}/Windows_Binaries"/{BootSectImage,GenBootSector}.exe || true
+	cp "${EDK2_DIR}/BaseTools/Bin/Win32"/{BootSectImage,GenBootSector}.exe "${UEFI_DUET_INSTALLER_DIR}/Windows_Binaries/"
 	
 	echo
 	
 	cd "${EDK2_DIR}/"
-	rm -rf "${EFI_DUET_GIT_DIR}/UDK_EDK2_DuetPkg_Changes_to_Makefiles.patch" || true
-	git diff remotes/origin/master...keshav_pr > "${EFI_DUET_GIT_DIR}/UDK_EDK2_DuetPkg_Changes_to_Makefiles.patch"
+	rm -rf "${UEFI_DUET_INSTALLER_DIR}/UDK_EDK2_DuetPkg_Changes_to_Makefiles.patch" || true
+	git diff remotes/origin/master...keshav_pr > "${UEFI_DUET_INSTALLER_DIR}/UDK_EDK2_DuetPkg_Changes_to_Makefiles.patch"
 	
 	echo
 	
-	cd "${EFI_DUET_GIT_DIR}/"
+	cd "${UEFI_DUET_INSTALLER_DIR}/"
 	git add *
 	git status
 	git commit -a -m "$(date +%d-%b-%Y)" || true
@@ -125,13 +125,13 @@ MEMDISK_COMPILED_GIT() {
 	echo "Tiano_DUET_memdisk_compiled_GIT"
 	echo
 	
-	rm -rf "${MEMDISK_COMPILED_DIR}/Tiano_EDK2_DUET_X64.img" || true
-	cp "${DUET_EMUVARIABLE_BUILD_DIR}/floppy.img" "${MEMDISK_COMPILED_DIR}/Tiano_EDK2_DUET_X64.img"
-	# cp "${WD}/duet_x64_memdisk.bin" "${MEMDISK_COMPILED_DIR}/Tiano_EDK2_DUET_X64.img"
+	rm -rf "${DUET_MEMDISK_COMPILED_DIR}/Tiano_EDK2_DUET_X64.img" || true
+	cp "${DUET_EMUVARIABLE_BUILD_DIR}/floppy.img" "${DUET_MEMDISK_COMPILED_DIR}/Tiano_EDK2_DUET_X64.img"
+	# cp "${WD}/duet_x64_memdisk.bin" "${DUET_MEMDISK_COMPILED_DIR}/Tiano_EDK2_DUET_X64.img"
 	
 	echo
 	
-	cd "${MEMDISK_COMPILED_DIR}/"
+	cd "${DUET_MEMDISK_COMPILED_DIR}/"
 	git add *
 	git status
 	git commit -a -m "$(date +%d-%b-%Y)" || true
@@ -205,9 +205,9 @@ unset EDK2_DUET_BOOTSECT_BIN_DIR
 unset EDK2_BUILD_DIR
 unset DUET_EMUVARIABLE_BUILD_DIR
 unset DUET_FSVARIABLE_BUILD_DIR
-unset COMPILED_DIR
-unset EFI_DUET_GIT_DIR
-unset MEMDISK_COMPILED_DIR
+unset DUET_COMPILED_DIR
+unset UEFI_DUET_INSTALLER_DIR
+unset DUET_MEMDISK_COMPILED_DIR
 unset MEMDISK_DIR
 unset MIGLE_BOOTDUET_COMPILE_DIR
 unset SRS5694_DUET_INSTALL_DIR
