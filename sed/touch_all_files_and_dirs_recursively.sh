b5d466c6fc9d9cb605d36c35c5534af1972cfe99
Keshav Amburay <the.ridikulus.rat@gmail.com>
2011-07-10 01:12:51 +0530

    sed: script to modify file timestamps using touch utility

---
+++ sed/touch_all_files_and_dirs_recursively.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+RUN()
+{
+	
+	name=""
+	
+	ls --all -1 | while read -r name
+	do
+		echo
+		
+		if [ ! -d "${PWD}/${name}" ] && [ "${name}" != "." ] && [ "${name}" != ".." ]
+		then
+			
+			echo
+			
+			echo "Current Directory : ${PWD}"
+			
+			echo
+			
+			touch --no-create "${name}"
+			
+			echo
+			
+		fi
+		
+		echo
+		
+		if [ -d "${PWD}/${name}" ] && [ "${name}" != "." ] && [ "${name}" != ".." ] && [ ! "$(file "${PWD}/${name}" | grep 'symbolic link to')" ]
+		then
+			pushd "${name}" > /dev/null
+			RUN
+			popd > /dev/null
+		fi
+		
+		echo
+		
+	done
+	
+}
+
+set -x -e
+
+echo
+
+RUN
+
+echo
+
+set +x +e
+
+echo
+
+unset name
