63b5cd3be5c18615e3a6da1a9b002bba6f3874e4
Keshav Amburay <the.ridikulus.rat@gmail.com>
2012-02-16 11:57:43 +0530

    Add falconindy's pkgbuild upg chksums script

---
+++ archlinux/pkgbuild_upg_chksums.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+if [[ -z "${1}" ]]; then
+	exit 1
+fi
+
+_OPTION="${1}"
+
+## https://wiki.archlinux.org/index.php/Makepkg#Replace_checksums_in_PKGBUILD_automatically
+
+if [[ "${_OPTION}" == "1" ]]; then
+	
+	## Option 1
+	
+	## Script by Falconindy
+	## https://bbs.archlinux.org/viewtopic.php?id=131666
+	
+	awk -v newsums="$(makepkg -g)" '
+BEGIN {
+  if (!newsums) exit 1
+}
+
+/^[[:blank:]]*(md|sha)[[:digit:]]+sums=/,/\)[[:blank:]]*$/ {
+  if (!i) print newsums; i++
+  next
+}
+
+1
+' "${PWD}/PKGBUILD" > "${PWD}/PKGBUILD.new" && mv "${PWD}"/PKGBUILD{.new,}
+	
+fi
+
+if [[ "${_OPTION}" == "2" ]]; then
+	
+	## Option 2
+	
+	setconf "${PWD}/PKGBUILD" $(makepkg -g 2>/dev/null | pee "head -1 | cut -d= -f1" "cut -d= -f2") ')'
+	
+fi
