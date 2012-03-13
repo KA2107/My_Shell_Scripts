e67299cce1700d8ea598a7f0c2acb63d8e589c02
Keshav Amburay <the.ridikulus.rat@gmail.com>
2012-03-13 19:30:49 +0530

    git: Fix Change Author in GIT repo script

--- git/git_change_author_repo.sh
+++ git/git_change_author_repo.sh
@@ -1,21 +1,22 @@
 #!/usr/bin/env bash
 
-OLD_AUTHOR_NAME="${1}"
-OLD_AUTHOR_EMAIL="${2}"
+export NEW_AUTHOR_NAME="${1}"
+export NEW_AUTHOR_EMAIL="${2}"
 
-OLD_COMMITTER_NAME="${OLD_AUTHOR_NAME}"
-OLD_COMMITTER_EMAIL="${OLD_AUTHOR_EMAIL}"
+export NEW_COMMITTER_NAME="${NEW_AUTHOR_NAME}"
+export NEW_COMMITTER_EMAIL="${NEW_AUTHOR_EMAIL}"
 
-NEW_AUTHOR_NAME="${3}"
-NEW_AUTHOR_EMAIL="${4}"
+export OLD_AUTHOR_NAME="${3}"
+export OLD_AUTHOR_EMAIL="${4}"
 
-NEW_COMMITTER_NAME="${NEW_AUTHOR_NAME}"
-NEW_COMMITTER_EMAIL="${NEW_AUTHOR_EMAIL}"
+export OLD_COMMITTER_NAME="${OLD_AUTHOR_NAME}"
+export OLD_COMMITTER_EMAIL="${OLD_AUTHOR_EMAIL}"
+
 
 ## Taken from http://help.github.com/change-author-info/
 ## Ideas from http://stackoverflow.com/questions/750172/how-do-i-change-the-author-of-a-commit-in-git
 
-git filter-branch --env-filter "
+git filter-branch --env-filter '
 
 an="${GIT_AUTHOR_NAME}"
 am="${GIT_AUTHOR_EMAIL}"
@@ -37,5 +38,16 @@ export GIT_AUTHOR_EMAIL="${am}"
 export GIT_COMMITTER_NAME="${cn}"
 export GIT_COMMITTER_EMAIL="${cm}"
 
-"
+'
 
+unset NEW_AUTHOR_NAME
+unset NEW_AUTHOR_EMAIL
+
+unset NEW_COMMITTER_NAME
+unset NEW_COMMITTER_EMAIL
+
+unset OLD_AUTHOR_NAME
+unset OLD_AUTHOR_EMAIL
+
+unset OLD_COMMITTER_NAME
+unset OLD_COMMITTER_EMAIL
