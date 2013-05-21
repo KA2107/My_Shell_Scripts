2aad18f4030b6cf7698bb824a1277c252a0a90f8
Keshav Amburay <the.ridikulus.rat@gmail.com>
2013-05-21 08:48:53 +0530

    Fix compile_kernel_module.sh script

---
+++ linux/kernel/compile_kernel_module.sh
@@ -0,0 +1,96 @@
+#!/usr/bin/env bash
+
+__WD="${__SOURCE_CODES_PART__}/Source_Codes/Operating_Systems/Linux"
+_WD="${__WD}/Linux_Kernel_Mainline_GIT/"
+
+_KERNEL_VER_ACTUAL="3.10-rc1"
+_KERNEL_VER_PATH="3.10.0-1-mainline"
+
+_KERNEL_CONFIG_OPTIONS="CONFIG_EFIVAR_FS=m"
+_KERNEL_SUBDIR="fs/efivarfs"
+_KERNEL_MODULE_FILENAME="efivarfs"
+
+echo
+
+set -x -e
+
+echo
+
+cd "${_WD}/"
+
+echo
+
+git reset --hard
+
+echo
+
+git clean -f
+
+echo
+
+git checkout "v${_KERNEL_VER_ACTUAL}"
+echo
+
+install -D -m0644 "/usr/lib/modules/${_KERNEL_VER_PATH}/build/Module.symvers" "${_WD}/Module.symvers"
+
+zcat /proc/config.gz > "${_WD}/.config"
+# install -D -m0644 "/usr/src/${_KERNEL_VER_PATH}/.config" "${_WD}/.config"
+
+echo
+
+# make xconfig
+
+echo
+
+echo
+
+yes "" | make config > /dev/null
+
+echo
+
+make modules_prepare
+
+echo
+
+make -C "${_WD}/" ${_KERNEL_CONFIG_OPTIONS} SUBDIRS="${_KERNEL_SUBDIR}" modules
+
+echo
+
+sudo rm -f "/usr/lib/modules/${_KERNEL_VER_PATH}/kernel/${_KERNEL_SUBDIR}/${_KERNEL_MODULE_FILENAME}.ko" || true
+sudo rm -f "/usr/lib/modules/${_KERNEL_VER_PATH}/kernel/${_KERNEL_SUBDIR}/${_KERNEL_MODULE_FILENAME}.ko.gz" || true
+
+echo
+
+gzip "${_WD}/${_KERNEL_SUBDIR}/${_KERNEL_MODULE_FILENAME}.ko"
+
+sudo install -D -m0644 "${_WD}/${_KERNEL_SUBDIR}/${_KERNEL_MODULE_FILENAME}.ko.gz" "/usr/lib/modules/${_KERNEL_VER_PATH}/kernel/${_KERNEL_SUBDIR}/${_KERNEL_MODULE_FILENAME}.ko.gz"
+
+echo
+
+make clean
+
+rm -f "${_WD}/${_KERNEL_SUBDIR}/Module.symvers"
+rm -f "${_WD}/Module.symvers"
+rm -f "${_WD}/.config"
+
+echo
+
+git reset --hard
+
+echo
+
+git checkout master
+
+echo
+
+sudo depmod -Ae
+
+echo
+
+sudo modprobe ${_KERNEL_MODULE_FILENAME}
+
+echo
+
+set +x +e
+
+echo
